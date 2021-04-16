f17a1fd602dffc66c61b28cedf011d960bc4c7a2
Kerry <cobbkerry@gmail.com>
2021-04-15 23:41:38 -0500

    Add functions for sorting by similarity

--- msprimeSim.py
+++ msprimeSim.py
@@ -1,8 +1,5 @@
-from timeit import default_timer as timer
-import datetime
 import msprime
 import numpy as np
-from tqdm import tqdm
 from sklearn.neighbors import NearestNeighbors
 
 def simSeqData(seed, nDataSets, nSamples, sequenceLength, mutationRate):
@@ -10,66 +7,47 @@ def simSeqData(seed, nDataSets, nSamples, sequenceLength, mutationRate):
     lowerPopSize = 1e3
     upperPopSize = 1e5
     recombRate = 1e-8
-
-    print("Simulating datasets:")
-    print(f"\tseed: {seed}")
-    print(f"\tnumber of data sets: {nDataSets}")
-    print(f"\tsequence length: {sequenceLength}")
-    print(f"\tmutationRate: {mutationRate}")
-    print(f"\tpopulation size range: {lowerPopSize}-{upperPopSize}")
-    print(f"\trecombination rate: {recombRate}")
-
     # Create empty numpy arrays to hold data
-    populationSizeArray = np.empty(nDataSets, dtype=np.int64)
-    variantCharacterMatrixArray = np.empty(nDataSets, dtype=object)
-    invariantCharacterMatrixArray = np.zeros((nDataSets, sequenceLength, nSamples * 2), dtype=np.int8)
-    for rep in tqdm(range(nDataSets)):
+    popSizeArray = np.empty(nDataSets, dtype=np.int64)
+    varCharMatrixArray = np.empty(nDataSets, dtype=object)
+    invarCharMatrixArray = np.zeros((nDataSets, sequenceLength, 
+            nSamples * 2), dtype=np.int8)
+    for rep in range(nDataSets):
         # Draw random population size 
-        populationSize = np.random.randint(lowerPopSize, upperPopSize)
-        populationSizeArray[rep] = populationSize 
-
+        popSize = np.random.randint(lowerPopSize, upperPopSize)
+        popSizeArray[rep] = popSize 
         # Create tree sequences with msprime 
-        ts = msprime.sim_ancestry(samples=nSamples, recombination_rate=recombRate, 
-                sequence_length=sequenceLength, population_size=populationSize, 
-                random_seed=seed)
-        mutated_ts = msprime.sim_mutations(ts, rate=mutationRate, random_seed=seed, 
-                    model=msprime.BinaryMutationModel())
-
+        ts = msprime.sim_ancestry(samples=nSamples, 
+                recombination_rate=recombRate, sequence_length=sequenceLength, 
+                population_size=popSize, random_seed=seed)
+        mutated_ts = msprime.sim_mutations(ts, rate=mutationRate, 
+                random_seed=seed, model=msprime.BinaryMutationModel())
         # Get data matrix with only variable sites
-        variantCharacterMatrixArray[rep] = mutated_ts.genotype_matrix()
-        
+        varCharMatrixArray[rep] = mutated_ts.genotype_matrix()
         # Get complete character matrix with invariant sites
         for i in mutated_ts.variants():
             site = int(i.site.position)
-            invariantCharacterMatrixArray[rep][site,:] = i.genotypes
-    return (populationSizeArray, variantCharacterMatrixArray, invariantCharacterMatrixArray)
+            invarCharMatrixArray[rep][site,:] = i.genotypes
+    return (popSizeArray, varCharMatrixArray, invarCharMatrixArray)
 
-def sort_min_diff(amat):
-    """
-    This function takes in a SNP matrix with individuals on rows and returns 
-    the same matrix with individuals sorted by genetic similarity.
-    This problem is NP, so here we use a nearest neighbors approximation. It"s 
-    not perfect, but it"s fast and generally performs ok. This assumes your 
-    input matrix is a numpy array.
+def similarity_ranks(matrices):
+    ranks = np.empty(matrices.shape[0], dtype=object)
+    for i, mat in enumerate(matrices):
+        mat = mat.transpose() 
+        nbrs = NearestNeighbors(n_neighbors=mat.shape[0], metric="manhattan").fit(mat)
+        distances, ixs = nbrs.kneighbors(mat)
+        smallest = np.argmin(distances.sum(axis=1))
+        ranks[i] = ixs[smallest]
+    return ranks 
 
-    Taken from Flagel et al. 2019, doi:10.1093/molbev/msy224 
-    """ 
-    # TODO: Rewrite to sort on columns
-    # TODO: Make len(amat) more efficient
-    # print(len(amat))
-    # print(amat.shape)
-    mb = NearestNeighbors(len(amat), metric="manhattan").fit(amat)
-    v = mb.kneighbors(amat)
-    smallest = np.argmin(v[0].sum(axis=1))
-    return amat[v[1][smallest]]
+def sort_matrices(ranks, matrices):
+    for i in range(matrices.shape[0]):
+        matrices[i] = matrices[i][:,ranks[i]]
 
 def padMatrices(matrices):
-    maxLen = 0
-    nSamples = matrices[0].shape[1] 
-    for i in matrices:
-        if i.shape[0] > maxLen:
-            maxLen = i.shape[0]
-    paddedMatrixArray = np.zeros((matrices.shape[0], maxLen, nSamples), dtype=np.int8)
+    maxLen = max([i.shape[0] for i in matrices]) 
+    paddedMatrixArray = np.zeros((matrices.shape[0], maxLen, 
+            matrices[0].shape[1]), dtype=np.int8)
     for i in range(matrices.shape[0]):
         for j in range(matrices[i].shape[0]):
             paddedMatrixArray[i][j] = matrices[i][j]
@@ -77,32 +55,24 @@ def padMatrices(matrices):
 
 #-------------------------------------------------------------------------------
 # Simulate data with msprime
-start1 = timer()
-populationSizeArray, variantCharacterMatrixArray, invariantCharacterMatrixArray = simSeqData(
+popSizeArray, varCharMatrixArray, invarCharMatrixArray = simSeqData(
     seed=1234, 
-    nDataSets=5000, 
-    nSamples=20, 
-    # sequenceLength=50_000, 
-    sequenceLength=10_000, 
-    mutationRate=1e-7)
-end1 = timer()
-elapsed1 = str(datetime.timedelta(seconds=round(end1 - start1)))
-print(f"Elapsed time for data simulation: {elapsed1}")
+    nDataSets=1, 
+    nSamples=2, 
+    sequenceLength=6, 
+    mutationRate=1e-4)
 
-#TODO: Sort matrices by similarity
+# Sort matrices by similarity
+ranks = similarity_ranks(varCharMatrixArray)
+sort_matrices(ranks, varCharMatrixArray)
+sort_matrices(ranks, invarCharMatrixArray)
 
 # Pad variant matices with zeros
-paddedVariantCharacterMatrixArray = padMatrices(variantCharacterMatrixArray)
+padVarCharMatrixArray = padMatrices(varCharMatrixArray)
 
-# Split data sets into testing and training
-outputFile = f"simulated-data-{invariantCharacterMatrixArray[0].shape[0]}.npz"
-print(f"Saving compressed data to {outputFile}")
-start2 = timer()
+# Compress and save data 
 np.savez_compressed(
-    outputFile,
-    populationSizeArray=populationSizeArray,
-    variantMatrixArray=paddedVariantCharacterMatrixArray,
-    invariantMatrixArray=invariantCharacterMatrixArray)
-end2 = timer()
-elapsed2 = str(datetime.timedelta(seconds=round(end1 - start1)))
-print(f"Elapsed time for data compression and output: {elapsed2}")
+    "simulated-data.npz",
+    populationSizeArray=popSizeArray,
+    variantMatrixArray=padVarCharMatrixArray,
+    invariantMatrixArray=invarCharMatrixArray)
